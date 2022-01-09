e9249d1e063a36f59be965b47cdac0257657006a
kd444 <karkeradarshan98@gmail.com>
2022-01-09 13:59:34 +0530

    added components and function ,add to do

--- src/App.js
+++ src/App.js
@@ -1,101 +1,59 @@
 // import logo from './logo.svg';
 import "./App.css";
+import Header from "./MyComponents/Header";
+import { Todos } from "./MyComponents/Todos";
+import { Footer } from "./MyComponents/Footer";
+import AddTodo from "./MyComponents/AddTodo";
 
+import React, { useState } from "react";
 function App() {
+    const onDelete = (todo) => {
+        console.log("i am on delete of to do ", todo);
+        // Deleting this way in react doesnt work
+        // let index = todos.indexOf(todo);
+        //todos.splice(index, 1);
+        setTodos(
+            todos.filter((e) => {
+                return e !== todo;
+            })
+        );
+    };
+    const addTodo = (title, desc) => {
+        console.log("i am adding this todo", title, desc);
+        let sno = todos[todos.length - 1].sno + 1;
+        const myTodo = {
+            sno: sno,
+            title: title,
+            desc: desc,
+        };
+        setTodos([...todos, myTodo]);
+        console.log(myTodo);
+    };
+    const [todos, setTodos] = useState([
+        {
+            sno: 1,
+            title: "Go to the  market ",
+            desc: "You need to go the market to get this job done ",
+        },
+        {
+            sno: 2,
+            title: "Go to the  mall ",
+            desc: "You need to go the mall to get this job done ",
+        },
+        {
+            sno: 3,
+            title: "Go to the university ",
+            desc: "You need to go the university to get this job done ",
+        },
+    ]);
+
     return (
         // jsx
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        Navbar
-                    </a>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div
-                        className="collapse navbar-collapse"
-                        id="navbarSupportedContent"
-                    >
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a
-                                    className="nav-link active"
-                                    aria-current="page"
-                                    href="#"
-                                >
-                                    Home
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">
-                                    Link
-                                </a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a
-                                    className="nav-link dropdown-toggle"
-                                    href="#"
-                                    id="navbarDropdown"
-                                    role="button"
-                                    data-bs-toggle="dropdown"
-                                    aria-expanded="false"
-                                >
-                                    Dropdown
-                                </a>
-                                <ul
-                                    className="dropdown-menu"
-                                    aria-labelledby="navbarDropdown"
-                                >
-                                    <li>
-                                        <a className="dropdown-item" href="#">
-                                            Action
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a className="dropdown-item" href="#">
-                                            Another action
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <hr className="dropdown-divider" />
-                                    </li>
-                                    <li>
-                                        <a className="dropdown-item" href="#">
-                                            Something else here
-                                        </a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled">Disabled</a>
-                            </li>
-                        </ul>
-                        <form className="d-flex">
-                            <input
-                                className="form-control me-2"
-                                type="search"
-                                placeholder="Search"
-                                aria-label="Search"
-                            />
-                            <button
-                                className="btn btn-outline-success"
-                                type="submit"
-                            >
-                                Search
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </nav>
+            <Header title="My Todos List" searchbar={true} />
+            <AddTodo addTodo={addTodo} />
+            <Todos todos={todos} onDelete={onDelete} />
+            <Footer />
         </>
     );
 }
